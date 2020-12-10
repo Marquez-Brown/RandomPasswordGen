b463f815571f654a2091194cdc4268387f924388
Juno Brown <amarbrown@gmail.com>
2020-12-10 16:46:22 -0500

    added write password function call, deleted extra if statement that was not needed

--- script.js
+++ script.js
@@ -29,8 +29,6 @@ function generatePassword() {
 
   if (length < 8 && length > 128) {
     return alert("password MUST be between 8 and 128 characters")
-  }
-  if (length > 8 && length < 129 ) {
 
   }
   if (numeric) {
@@ -46,58 +44,12 @@ function generatePassword() {
     possibleChars = possibleChars + special
   }
 }
-  //make if statements for the rest of the possibilities
-  }
-  var generatedPassword = "";
-  for (i = 0; i < length; i++) {
-    generatedPassword += possibleChars[Math.floor(Math.random() * possibleChars.length)]
-   return generatedPassword
-  }
+//make if statements for the rest of the possibilities
+
+var generatedPassword = "";
+for (i = 0; i < length; i++) {
+  generatedPassword += possibleChars[Math.floor(Math.random() * possibleChars.length)]
+  
 }
-
-
-// WHEN prompted for the length of the password
-// WHEN prompted for the length of the password
-// WHEN prompted for the length of the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-
-
-// function getRandomLower() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-// }
-
-// function getRandomUpper() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-// }
-// function getRandomNumber() {
-//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-// }
-// function getRandomSpecChar() {
-//   const symbols = '!@#$%^&*(){}[]+<>/,.';
-//   return symbols[Math.floor(Math.random() * symbols.length)];
-// }
-
-// console.log(getRandomSpecChar());
-
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-// Browser character set
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// document.getElementById.onclick("#generate").questions();
-
-
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
+//run writePassword to fill out text box
+writePassword()
