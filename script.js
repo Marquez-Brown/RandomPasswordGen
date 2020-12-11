d3425e3beecf3eb947d2e2faa96b028752287613
Juno Brown <amarbrown@gmail.com>
2020-12-10 20:39:37 -0500

    added comments explaining all of the code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// var uppercase, lowercase, special, numeric, length, possibleChars;
-//make variables that are strings containing every letter possiblity
+//var for generating random password
 var numbers = "1234567890"
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -9,7 +8,7 @@ var special = "!@#$%^&*(){}[]+<>/,."
 var length = ""
 var possibleChars = ""
 var generatedPassword = ""
-// var password = generatedPassword
+// function writePassword adds the password to the text box
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -18,43 +17,39 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+//Creates randomize password
 function generatePassword() {
   var addUppercase = confirm("Do you want Uppercase Letters?");
   var addLowercase = confirm("Do you want lowercase letters?");
   var addNumeric = confirm("Do you want Random Numbers?");
   var addSpecial = confirm("Do you want Special Characters?");
   length = prompt("Choose between 8 and 128 Characters");
-
+  //if password isnt within parameters it will send alert
   if (length < 8 || length > 128) {
     return alert("password MUST be between 8 and 128 characters")
 
-  }
+  }//adds numbers
   if (addNumeric) {
-    possibleChars =  possibleChars + numbers
-  
-  }
+    possibleChars = possibleChars + numbers
+
+  }//adds uppercase chars
   if (addUppercase) {
     possibleChars = possibleChars + uppercase
-  }
+  }//adds lowercase chars
   if (addLowercase) {
     possibleChars = possibleChars + lowercase
-  }
+  }//adds special chars
   if (addSpecial) {
     possibleChars = possibleChars + special
   }
-  
+//for look to create random password
   for (i = 0; i < length; i++) {
     generatedPassword += possibleChars[Math.floor(Math.random() * possibleChars.length)]
-    
+
   }
-  
+//sends password to var generatedPassword
   return generatedPassword
 }
-// Add event listener to generate button
+// tells function writePassword to write the password in textfield
 generateBtn.addEventListener("click", writePassword);
-//make if statements for the rest of the possibilities
 
-
-
-//run writePassword to fill out text box
